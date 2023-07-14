6aabe9337fbe8078ef8e2c7f84d6f7bf3fbba19a
Juan Betancourt <josebetancourt09@hotmail.com>
2023-07-14 04:05:15 -0600

    modal incluido

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
 });
+
+// Modal
+const openModal = document.querySelector('.modal__btn');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
